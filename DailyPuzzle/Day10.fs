afe63ed5ee8974ff9a4bdbac33772f8360574208
Linus Vettiger <lv@vlot.ch>
2022-12-13 01:24:01 +0100

    2/2 Day 10

--- DailyPuzzle/Day10.fs
+++ DailyPuzzle/Day10.fs
@@ -13,6 +13,9 @@ let private parseLine (input: string) : Instr option =
     | Util.Regex "addx (-?\d+)" [ Util.Int num ] -> Some (Instr.AddX num)
     | _ -> None
 
+let private emptyLine : char array =
+    Array.init 40 (fun _ -> '.')
+
 let private compute (state: int, intermediate: int array) (next: Instr) =
     match next with
     | Instr.NoOp -> state, Array.append intermediate [| state |]
@@ -27,17 +30,36 @@ let private sample (offset: int) (every: int) (array: int array) : (int * int) a
     Array.map (Array.get array) indices
     |> Array.zip indices
 
+let private printData (steps: int array) =
+    let mapper (i: int) (nextState: int) =
+        if i = nextState - 1 || i = nextState || i = nextState + 1 then
+            '#'
+        else
+            '.'
+    
+    Array.mapi mapper steps
+    |> System.String
+
 let private part1 () =
     let data = Data.load phase today 1
     data
     |> Array.choose parseLine
     |> Array.fold compute (1, [| 1 |])
-    |> (snd >> sample 20 40)
+    |> snd
+    |> sample 20 40
     |> Array.fold (fun sum (x, y) -> sum + (x + 1) * y) 0
 
 let private part2 () =
-    -1
+    let data = Data.load phase today 1
+    data
+    |> Array.choose parseLine
+    |> Array.fold compute (1, [| 1 |])
+    |> snd
+    |> fun a -> Array.removeAt (Array.length a - 1) a // superfluous last element
+    |> Array.chunkBySize 40
+    |> Array.map printData
+    |> String.concat "\n"
 
 let run () =
     part1 () |> printfn "TODO: \"%A\""
-    part2 () |> printfn "TODO: \"%A\""
+    part2 () |> printfn "TODO: \n%s"
